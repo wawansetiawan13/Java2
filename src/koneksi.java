f57fb7fca4edff7c63b2a55e996dcdf8dc56690f
wawansetiawan13 <wawanmraz1331@gmail.com>
2020-04-05 16:09:54 -0700

    first commit

---
+++ src/koneksi.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class koneksi {
+    public Connection bukakoneksi (){
+        Connection con=null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con= (Connection)DriverManager.getConnection("jdbc:mysql://localhost/java2db","root","");
+            System.out.println("berhasil");
+            return con;
+        }
+        catch (Exception e){
+            System.out.println("gagal");
+            return null;
+            
+        }
+    }
+    public static void main (String args[]){
+        new koneksi().bukakoneksi();
+    }
+}
